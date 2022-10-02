c3019471f6ce191879fb479681294f2af3db4020
nuong dang <dangthinuong1804@gmail.com>
2022-10-02 12:26:58 +0700

    bai5

---
+++ video5/video5.js
@@ -0,0 +1,9 @@
+//console.log(`Hello world from javascript`)
+let text = `please visit Microsoft `
+text = text.replace("Microsoft", "W3school")
+console.log(text)
+//strim hàm loại bỏ những khoảng trắng không cần thiết 
+
+let text1 = '    Hello World from javascript!!!!!       '
+text1 = text1.trim()
+console.log(text1)
