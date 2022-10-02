7733a2ddbd606761234f6110342c065434eff224
nuong dang <dangthinuong1804@gmail.com>
2022-10-02 12:45:20 +0700

    bai6

---
+++ video6/video6.js
@@ -0,0 +1,15 @@
+console.log('Hello world from HTML'.toUpperCase())
+let a = 5;
+let b = 6;
+let c = 6.9
+let d = '5'
+let e = 'hello'
+let g = +d//đây là chuyển kiểu từ string sang number 
+console.log(a+b)
+console.log(a-d)
+console.log(a/e)
+console.log(a === d,'a:',typeof a,'b: ',typeof b)
+console.log('g value: ',g,'type: ', typeof g)
+
+
+
